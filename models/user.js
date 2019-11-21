42da9c77917fedfabf40161c0116cfd5b1512580
lucaschristopher <lucas.christopher.silva@ccc.ufcg.edu.br>
2019-11-21 15:42:56 -0300

    4th class: Creating Mongoose Schemas

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  return res.send({ message: "Alright with the users's GET method." });
-});
+const Users = require("../models/user");
 
-router.post("/", (req, res) => {
-  return res.send({ message: "Alright with the users's POST method." });
+router.get("/", (req, res) => {
+  Users.find({}, (err, data) => {
+    if (err) return res.send({ error: "Error querying users..." });
+    return res.send(data);
+  });
 });
 
 router.post("/create", (req, res) => {
-  return res.send({ message: "Successfully created user!" });
+  const { email, password } = req.body;
+
+  if (!email || !password) return res.send({ error: "Insufficient data..." });
+
+  Users.findOne({ email }, (err, data) => {
+    if (err) return res.send({ error: "Error searching user..." });
+    if (data) return res.send({ error: "User already registered!" });
+  });
+
+  Users.create(req.body, (err, data) => {
+    if (err) return res.send({ error: "Error creating user..." });
+
+    return res.send(data);
+  });
 });
 
 module.exports = router;
