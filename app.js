83e0b5c9395c6b14d0f8c99b51d37dea556f5a6a
lucaschristopher <lucas.christopher.silva@ccc.ufcg.edu.br>
2019-11-21 15:01:07 -0300

    3rd class: Connecting MongoDB with Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express();
 
+// MongoDB Atlas - Cloud DBaaS for MongoDB
+// String of DB connection
+const url =
+  "mongodb+srv://<admin>:<password>@cluster-api-node-pndno.mongodb.net/test?retryWrites=true&w=majority";
+
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  poolSize: 5,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+mongoose.connect(url, options);
+mongoose.set("useCreateIndex", true);
+
+// Some events for Mongo to stay listening and report if something different happened
+
+mongoose.connection.on("connected", () => {
+  console.log("Application connected on database =D")
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Application disconnected from database.");
+});
+
+mongoose.connection.on("error", err => {
+  console.log(`Error on connection with database: ${err}`);
+});
+
+// Body Parser
+// Get requested object as JSON
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
 const indexRoute = require("./routes/index");
 const usersRoute = require("./routes/users");
 
