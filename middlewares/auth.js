fc936398c6d1f78479324be4633244768c50898f
Lucas Christopher <lucas.christopher.silva@ccc.ufcg.edu.br>
2019-12-20 19:47:53 -0300

    8th class: Returning appropriate status code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,12 @@ const auth = (req, res, next) => {
   const tokenHeader = req.headers.auth;
 
   if (!tokenHeader)
-    return res.send({ error: "Authentication refused: token not sent!" });
+    return res
+      .status(401)
+      .send({ error: "Authentication refused: token not sent!" });
 
   jwt.verify(tokenHeader, "nossacaraquelegal", (error, decoded) => {
-    if (error) return res.send({ error: "Invalid token!" });
+    if (error) return res.status(401).send({ error: "Invalid token!" });
     res.locals.authData = decoded;
     return next();
   });
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
 const router = express.Router();
 
 const Users = require("../models/user");
@@ -18,42 +19,45 @@ router.get("/", async (req, res) => {
     const users = await Users.find({});
     return res.send(users);
   } catch (error) {
-    return res.send({ error: "Error querying users..." });
+    return res.status(500).send({ error: "Error querying users..." });
   }
 });
 
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) return res.send({ error: "Insufficient data..." });
+  if (!email || !password)
+    return res.status(400).send({ error: "Insufficient data..." });
 
   try {
     if (await Users.findOne({ email }))
-      return res.send({ error: "User already registered!" });
+      return res.status(400).send({ error: "User already registered!" });
 
     const user = await Users.create(req.body);
     user.password = undefined;
 
-    return res.send({ user, token: createUserToken(user.id) });
+    return res.status(201).send({ user, token: createUserToken(user.id) });
   } catch (error) {
-    return res.send({ error: "Error searching user..." });
+    return res.status(500).send({ error: "Error searching user..." });
   }
 });
 
 router.post("/auth", async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) return res.send({ error: "Insufficient data..." });
+  if (!email || !password)
+    return res.status(400).send({ error: "Insufficient data..." });
 
   try {
     const user = await Users.findOne({ email }).select("+password");
-    if (!user) return res.send({ error: "Unregistered user!" });
+    if (!user) return res.status(400).send({ error: "Unregistered user!" });
 
     const password_ok = await bcrypt.compare(password, user.password);
-    if (!password_ok) return res.send({ error: "Authentication error..." });
+    if (!password_ok)
+      return res.status(401).send({ error: "Authentication error..." });
 
     user.password = undefined;
     return res.send({ user, token: createUserToken(user.id) });
   } catch (error) {
-    return res.send({ error: "Error searching user..." });
+    return res.status(500).send({ error: "Error searching user..." });
   }
 });
 
